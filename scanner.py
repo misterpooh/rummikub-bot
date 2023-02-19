89e699434f3379fcf525c1c498eb66f7b669c59d
James Lee <jameslee10@live.com>
2023-02-19 10:12:54 -0500

    added board/player tile scanning

--- scanner.py
+++ scanner.py
@@ -18,6 +18,8 @@ class Scanner:
         self.game_top_left = None
         self.game_top_left = self.locate_game_window() # (x,y)
         self.initialized = False
+        self.board_top_left = None
+        self.player_top_left = None
 
     def locate_player(self, height=GAME_HEIGHT, width=GAME_WIDTH):
         if not self.game_top_left:
@@ -35,7 +37,7 @@ class Scanner:
         if not self.game_top_left:
             self.locate_game_window()
         turn_loc = pyautogui.locateOnScreen(Tiles.TURN.image, confidence=0.9, region=tuple([self.game_top_left[0], self.game_top_left[1], self.GAME_WIDTH, self.GAME_HEIGHT]), grayscale=True)
-        if turn_loc: #TODO: Check color and return true if orange
+        if turn_loc: 
             cross_button = pyautogui.center(turn_loc)
             print(cross_button)
             print(cross_button.x)
@@ -52,7 +54,7 @@ class Scanner:
         distance = pow(distance, 2)
         elements = []
         if top_left:
-            game_window = tuple([top_left[0], top_left[1], self.GAME_WIDTH, self.GAME_HEIGHT])
+            game_window = tuple([top_left[0], top_left[1], top_left[2], top_left[3]])
             for element in pyautogui.locateAllOnScreen(path, confidence=confidence, region=game_window):
                 if all(map(lambda x: pow(element.left - x.left, 2) + pow(element.top - x.top, 2) > distance, elements)):
                     elements.append(element)
@@ -75,181 +77,199 @@ class Scanner:
             corner_img_loc = pyautogui.locateOnScreen(Tiles.GAME_CORNER.image, confidence=0.9, grayscale=True)
             top_right = (corner_img_loc.left + corner_img_loc.width , corner_img_loc.top)
             top_left = (top_right[0] - width, top_right[1])
-        self.game_top_left = top_left        
+        self.game_top_left = (top_left[0], top_left[1], self.GAME_WIDTH, self.GAME_HEIGHT)        
 
-    def scan_all_tiles(self):
+    def scan_board_tiles(self):
+        if not self.board_top_left:
+            self.board_top_left = self.locate_board()
+        if not self.player_top_left:
+            self.player_top_left = self.player_top_left()
+        board_x = self.board_top_left[0]
+        board_y = self.board_top_left[1]
+        player_x = self.player_top_left[0]
+        player_y = self.player_top_left[1]
+        self.scan_all_tiles(loc=tuple((board_x, board_y, self.GAME_WIDTH - board_x + self.game_top_left[0], player_y - board_y)))
+        return
+    
+    def scan_player_tiles(self):
+        return
+    
+    def scan_all_tiles(self, loc=None):
+        if not loc:
+            loc = self.game_top_left
         start_time = datetime.datetime.now()
         self.locate_game_window()
+        tiles = [] # TODO: store all the tiles and positions
         print("Scanning Black")
         print("1")
-        for i in self.locate_all(Tiles.BLACK_ONE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_ONE.image, confidence=0.9, top_left=loc):
             print(i)
 
         print("2")
-        for i in self.locate_all(Tiles.BLACK_TWO.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_TWO.image, confidence=0.9, top_left=loc):
             print(i)
         print("3")
-        for i in self.locate_all(Tiles.BLACK_THREE.image, confidence=0.95, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_THREE.image, confidence=0.95, top_left=loc):
             print(i)
         print("4")
-        for i in self.locate_all(Tiles.BLACK_FOUR.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_FOUR.image, confidence=0.9, top_left=loc):
             print(i)
         print("5")
-        for i in self.locate_all(Tiles.BLACK_FIVE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_FIVE.image, confidence=0.9, top_left=loc):
             print(i)
         print("6")
-        for i in self.locate_all(Tiles.BLACK_SIX.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_SIX.image, confidence=0.9, top_left=loc):
             print(i)
         print("7")
-        for i in self.locate_all(Tiles.BLACK_SEVEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_SEVEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("8")
-        for i in self.locate_all(Tiles.BLACK_EIGHT.image, confidence=0.8, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_EIGHT.image, confidence=0.8, top_left=loc):
             print(i)
         print("9")
-        for i in self.locate_all(Tiles.BLACK_NINE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_NINE.image, confidence=0.9, top_left=loc):
             print(i)
         print("10")
-        for i in self.locate_all(Tiles.BLACK_TEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_TEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("11")
-        for i in self.locate_all(Tiles.BLACK_ELEVEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_ELEVEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("12")
-        for i in self.locate_all(Tiles.BLACK_TWELVE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_TWELVE.image, confidence=0.9, top_left=loc):
             print(i)
         print("13")
-        for i in self.locate_all(Tiles.BLACK_THIRTEEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_THIRTEEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("Joker")
-        for i in self.locate_all(Tiles.BLACK_JOKER.image, confidence=0.5, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLACK_JOKER.image, confidence=0.5, top_left=loc):
             print(i)
             
         print("Scanning Blue")
         print("1")
-        for i in self.locate_all(Tiles.BLUE_ONE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_ONE.image, confidence=0.9, top_left=loc):
             print(i)
         print("2")
-        for i in self.locate_all(Tiles.BLUE_TWO.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_TWO.image, confidence=0.9, top_left=loc):
             print(i)
         print("3")
-        for i in self.locate_all(Tiles.BLUE_THREE.image, confidence=0.95, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_THREE.image, confidence=0.95, top_left=loc):
             print(i)
         print("4")
-        for i in self.locate_all(Tiles.BLUE_FOUR.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_FOUR.image, confidence=0.9, top_left=loc):
             print(i)
         print("5")
-        for i in self.locate_all(Tiles.BLUE_FIVE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_FIVE.image, confidence=0.9, top_left=loc):
             print(i)
         print("6")
-        for i in self.locate_all(Tiles.BLUE_SIX.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_SIX.image, confidence=0.9, top_left=loc):
             print(i)
         print("7")
-        for i in self.locate_all(Tiles.BLUE_SEVEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_SEVEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("8")
-        for i in self.locate_all(Tiles.BLUE_EIGHT.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_EIGHT.image, confidence=0.9, top_left=loc):
             print(i)
         print("9")
-        for i in self.locate_all(Tiles.BLUE_NINE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_NINE.image, confidence=0.9, top_left=loc):
             print(i)
         print("10")
-        for i in self.locate_all(Tiles.BLUE_TEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_TEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("11")
-        for i in self.locate_all(Tiles.BLUE_ELEVEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_ELEVEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("12")
-        for i in self.locate_all(Tiles.BLUE_TWELVE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_TWELVE.image, confidence=0.9, top_left=loc):
             print(i)
         print("13")
-        for i in self.locate_all(Tiles.BLUE_THIRTEEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.BLUE_THIRTEEN.image, confidence=0.9, top_left=loc):
             print(i)
             
         print("Scanning Red")
         print("1")
-        for i in self.locate_all(Tiles.RED_ONE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_ONE.image, confidence=0.9, top_left=loc):
             print(i)
         print("2")
-        for i in self.locate_all(Tiles.RED_TWO.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_TWO.image, confidence=0.9, top_left=loc):
             print(i)
         print("3")
-        for i in self.locate_all(Tiles.RED_THREE.image, confidence=0.95, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_THREE.image, confidence=0.95, top_left=loc):
             print(i)
         print("4")
-        for i in self.locate_all(Tiles.RED_FOUR.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_FOUR.image, confidence=0.9, top_left=loc):
             print(i)
         print("5")
-        for i in self.locate_all(Tiles.RED_FIVE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_FIVE.image, confidence=0.9, top_left=loc):
             print(i)
         print("6")
-        for i in self.locate_all(Tiles.RED_SIX.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_SIX.image, confidence=0.9, top_left=loc):
             print(i)
         print("7")
-        for i in self.locate_all(Tiles.RED_SEVEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_SEVEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("8")
-        for i in self.locate_all(Tiles.RED_EIGHT.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_EIGHT.image, confidence=0.9, top_left=loc):
             print(i)
         print("9")
-        for i in self.locate_all(Tiles.RED_NINE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_NINE.image, confidence=0.9, top_left=loc):
             print(i)
         print("10")
-        for i in self.locate_all(Tiles.RED_TEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_TEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("11")
-        for i in self.locate_all(Tiles.RED_ELEVEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_ELEVEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("12")
-        for i in self.locate_all(Tiles.RED_TWELVE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_TWELVE.image, confidence=0.9, top_left=loc):
             print(i)
         print("13")
-        for i in self.locate_all(Tiles.RED_THIRTEEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_THIRTEEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("Joker")
-        for i in self.locate_all(Tiles.RED_JOKER.image, confidence=0.5, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.RED_JOKER.image, confidence=0.5, top_left=loc):
             print(i)
         
         
         print("Scanning Orange")
         print("1")
-        for i in self.locate_all(Tiles.ORANGE_ONE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_ONE.image, confidence=0.9, top_left=loc):
             print(i)
         print("2")
-        for i in self.locate_all(Tiles.ORANGE_TWO.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_TWO.image, confidence=0.9, top_left=loc):
             print(i)
         print("3")
-        for i in self.locate_all(Tiles.ORANGE_THREE.image, confidence=0.95, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_THREE.image, confidence=0.95, top_left=loc):
             print(i)
         print("4")
-        for i in self.locate_all(Tiles.ORANGE_FOUR.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_FOUR.image, confidence=0.9, top_left=loc):
             print(i)
         print("5")
-        for i in self.locate_all(Tiles.ORANGE_FIVE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_FIVE.image, confidence=0.9, top_left=loc):
             print(i)
         print("6")
-        for i in self.locate_all(Tiles.ORANGE_SIX.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_SIX.image, confidence=0.9, top_left=loc):
             print(i)
         print("7")
-        for i in self.locate_all(Tiles.ORANGE_SEVEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_SEVEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("8")
-        for i in self.locate_all(Tiles.ORANGE_EIGHT.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_EIGHT.image, confidence=0.9, top_left=loc):
             print(i)
         print("9")
-        for i in self.locate_all(Tiles.ORANGE_NINE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_NINE.image, confidence=0.9, top_left=loc):
             print(i)
         print("10")
-        for i in self.locate_all(Tiles.ORANGE_TEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_TEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("11")
-        for i in self.locate_all(Tiles.ORANGE_ELEVEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_ELEVEN.image, confidence=0.9, top_left=loc):
             print(i)
         print("12")
-        for i in self.locate_all(Tiles.ORANGE_TWELVE.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_TWELVE.image, confidence=0.9, top_left=loc):
             print(i)
         print("13")
-        for i in self.locate_all(Tiles.ORANGE_THIRTEEN.image, confidence=0.9, top_left=self.game_top_left):
+        for i in self.locate_all(Tiles.ORANGE_THIRTEEN.image, confidence=0.9, top_left=loc):
             print(i)
         time_elapsed = datetime.datetime.now() - start_time
         print("Completed in " + str(
